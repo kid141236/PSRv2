f3c118a150f60ee931527f2be2700aba4b364794
kid141236 <kid141236@gmail.com>
2016-11-06 13:53:38 +0800

    add toast

--- app/src/main/java/com/example/designarama/myapplication/MainActivity.java
+++ app/src/main/java/com/example/designarama/myapplication/MainActivity.java
@@ -34,7 +34,7 @@ public class MainActivity extends AppCompatActivity{
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), gameActivity.class);
                 startActivity(intent);
-                Toast.makeText(MainActivity.this,"game Start",Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this,"game Start",Toast.LENGTH_SHORT).show()；
                 soundPool.play(startsound, 1.0F, 1.0F, 0, 0, 1.0F);
 
             }
